ffcb04b6efe8e9dd3481c6481f65994eda4e8b24
maxgr <max.gritsch@outlook.com>
2020-10-30 12:15:16 +0100

    Hello word

--- src/main/java/jan.java
+++ src/main/java/jan.java
@@ -11,5 +11,6 @@
 public class jan {
     public static void main(String[] args){
         System.out.println("Ausgabe aus der main()-Methode");
+        System.out.println("aiaiaiaiaaiai");
     }
 }
