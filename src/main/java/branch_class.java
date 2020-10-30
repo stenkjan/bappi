71f3b7420280ae4ac66305f08fa61f40ac0fcf32
jst01 <jst01@DESKTOP-ARSUN0F.fritz.box>
2020-10-30 12:32:44 +0100

    main.class

---
+++ src/main/java/branch_class.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jst01
+ */
+public class branch_class {
+    String tachauch;
+}
